4baa254279c575b42d8f5d32a99152478bf96543
Matt Hampel <matth@localdata.com>
2016-02-22 18:07:31 -0500

    Move to authy

--- util/models.py
+++ util/models.py
@@ -1,11 +1,16 @@
+import datetime
 from peewee import *
 
 db = SqliteDatabase('carebot.db')
 
 class Story(Model):
     name = CharField(null = True)
-    slug = CharField()
-    tracking_started = DateTimeField()
+    slug = CharField(unique=True)
+    story_type = CharField(null=True)
+    url = CharField(null=True)
+    date = DateField(null=True)
+
+    tracking_started = DateTimeField(default=datetime.datetime.now)
     last_checked = DateTimeField(null = True)
 
     class Meta:
