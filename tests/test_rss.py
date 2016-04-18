bbb2903c8e05f3722558a4684d161010d4d700be
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-04-18 14:46:42 -0500

    Target date instead

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -23,4 +23,4 @@ class TestRSS(unittest.TestCase):
         scraper = RSSScraper(FakeSource)
         stories = scraper.scrape()
         self.assertEqual(stories[0]['name'], 'Carebot Design Principles')
-        self.assertEqual(type(stories[1]['article_posted']), datetime.datetime)
+        self.assertEqual(type(stories[1]['date']), datetime.datetime)
