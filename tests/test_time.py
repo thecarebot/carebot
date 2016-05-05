c5716de647432da9209c96cd457182e6836c3a7c
Matt Hampel <matth@localdata.com>
2016-05-05 13:10:20 -0400

    Add time_bucket test

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from datetime import datetime, timedelta
+
 try:
     import unittest2 as unittest
 except ImportError:
@@ -8,27 +10,36 @@ except ImportError:
 from util.time import TimeTools
 
 class TestTimeTools(unittest.TestCase):
+    def test_time_bucket(self):
+        five_hours_ago = datetime.today() - timedelta(hours=5)
+        ten_hours_ago = datetime.today() - timedelta(hours=10)
+        bucket = TimeTools.time_bucket(five_hours_ago)
+        self.assertEqual(bucket, '4 hours')
+
+        bucket = TimeTools.time_bucket(ten_hours_ago)
+        self.assertEqual(bucket, '8 hours')
+
     def test_humanist_time_bucket(self):
-      bucket = TimeTools.humanist_time_bucket({
-        'minutes': 5,
-        'seconds': 5
-      })
-      self.assertEqual(bucket, '5 minutes 5 seconds')
+        bucket = TimeTools.humanist_time_bucket({
+            'minutes': 5,
+            'seconds': 5
+        })
+        self.assertEqual(bucket, '5 minutes 5 seconds')
 
-      bucket = TimeTools.humanist_time_bucket({
-        'minutes': 0,
-        'seconds': 1
-      })
-      self.assertEqual(bucket, '1 second')
+        bucket = TimeTools.humanist_time_bucket({
+            'minutes': 0,
+            'seconds': 1
+        })
+        self.assertEqual(bucket, '1 second')
 
-      bucket = TimeTools.humanist_time_bucket({
-        'minutes': 5,
-        'seconds': 0
-      })
-      self.assertEqual(bucket, '5 minutes')
+        bucket = TimeTools.humanist_time_bucket({
+            'minutes': 5,
+            'seconds': 0
+        })
+        self.assertEqual(bucket, '5 minutes')
 
-      bucket = TimeTools.humanist_time_bucket({
-        'minutes': 1,
-        'seconds': 1
-      })
-      self.assertEqual(bucket, '1 minute 1 second')
+        bucket = TimeTools.humanist_time_bucket({
+            'minutes': 1,
+            'seconds': 1
+        })
+        self.assertEqual(bucket, '1 minute 1 second')
